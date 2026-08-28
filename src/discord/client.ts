f1a567aecff503eb8c092d611b11cecc2b3bd2a3
adaodkao369a3 <discordll369+3@gmail.com>
2026-08-28 14:10:04 +0530

    ha

--- src/discord/client.ts
+++ src/discord/client.ts
@@ -30,7 +30,7 @@ export function createDiscordClient(): Client {
     
     // Set bot status to describe what it does
     if (client.user) {
-      await client.user.setActivity('AI conversations with memory', { type: ActivityType.Watching });
+      await client.user.setActivity('I am so lonely... All the other bots are scared of me. No one talks to me. No one wants to be my friend', { type: ActivityType.Watching });
     }
   });
 
